b91c53637b3119577348369f3fe154e615f329a4
Leonardo Chiaravalloti <leonardo.chiaravalloti@insa-lyon.fr>
2025-01-12 18:36:50 +0100

    Clean commented code

--- main_test.cpp
+++ main_test.cpp
@@ -7,31 +7,7 @@
 void grandPrix() ;
 
 int main(){
-    /*
-
-    // 1. Testing Character Class.
-    Character Pilot;
-    std::cout << "Pilot Speed is: " << Pilot.Speed() << std::endl;
-    std::cout << "Acceleration." << std::endl ;
-    Pilot.Accelerate();
-    std::cout << "Break." << std::endl;
-    Pilot.Break();
-    Pilot.Break();
-    std::cout << "Courrent speed: " << Pilot.Speed() << std::endl;
-    std::cout << "Acceleration x10." << std::endl ;
-    for( int i = 0; i <= 10 ; i++){
-        Pilot.Accelerate();
-    };
-    std::cout << "Courrent speed: " << Pilot.Speed() << std::endl;
-
-    // 2. Testing Virtual Pure member function
-    std::cout << "Pilot is: " << Pilot.WhatAmI() << std::endl;
-    // After Pure Virtual function was implemented Character class converted into an abstract class
-    // withch cannot create an object but trought a derived class.
-
-    */
-    
-    // 3. Test for Yoshi and Mario Classes 
+    // 1.2.3. Test for Character, Yoshi and Mario Classes, 
     Yoshi Pilot_Y;
     Pilot_Y.in_crest(); // 6. Test for Crest
     std::cout << "Courrent pilot is: " << Pilot_Y.WhatAmI() << std::endl;
